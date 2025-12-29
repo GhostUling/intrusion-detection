37357b9cd55f8b471a783451dd1b834cf238978f
qingzi <3251864744@qq.com>
2025-12-29 16:42:45 +0800

    6

--- vue_cv/vue_ding/src/router/index.js
+++ vue_cv/vue_ding/src/router/index.js
@@ -43,8 +43,26 @@ const routes = [
   
 ]
 
+
 const router = new VueRouter({
   routes
 })
+
+//定义路由守卫，如果没有登录，则不允许用户访问
+router.beforeEach((to,from,next) => {
+  if(to.path === '/login'){
+    next();
+  }
+  if(to.path === '/register'){
+    next();
+  }
+  //从浏览器中获取当前登陆用户信息
+  const user = localStorage.getItem("user");
+  //如果访问的路径不是登录或注册，那么就要对用户的登录状态进行判断
+  if(!user && to.path !='/login' && to.path != '/register'){
+    return next("/login");
+  }
+  next();
+})    
 
 export default router
