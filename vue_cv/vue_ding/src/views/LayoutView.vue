bb81c28746f3840257b28c2114de7d42e9a1e1c4
Echo-Vibes <18391764356@163.com>
2025-12-29 11:21:31 +0800

    登录界面部分内容

--- vue_cv/vue_ding/src/views/LayoutView.vue
+++ vue_cv/vue_ding/src/views/LayoutView.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="home">
+  <div class="app">
     <el-container>
-      <el-header style="background-color: #b3c0d1">
-        <img
-          src="@/assets/logo.png"
-          style="width: 60px; height: 60px; position: relative; top: 7px"
-          alt=""
-        />
+      <el-header style="background-color: #545c64;">
+
+        <img src="@/assets/logo.png" style="width: 60px; height: 60px; position:absolute;left:10px;top:10px;">
         <!--   font-size: 18px    字体大小 -->
-        <span style="position: relative; top: -20px; left: 10px; font-size: 18px"
-          >Vue管理系统</span
-        >
+        <span style=" position:absolute;left:80px;top:30px;font-size: 18px;">Vue管理系统</span>
+
+        <!-- 退出登录组件 -->
+        <el-dropdown trigger="click" style="color: white; font-size: 16px; float: right; height:60px; line-height:60px">
+          <span class="el-dropdown-link">
+            {{ user.name }}<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <div @click="loginout()">退出登录</div>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+
+
       </el-header>
       <el-container>
-        <el-aside style="background-color: #545c64; width: 200px; min-height: 100vh">
-          <el-menu
-            :default-active="$route.path"
-            router
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b"
-          >
+        <el-aside style="background-color:#545c64; width:200px;min-height: 100vh; ">
+          <el-menu :default-active="$route.path" router class="el-menu-vertical-demo" @open="handleOpen"
+            @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
             <el-menu-item index="/">
               <i class="el-icon-menu"></i>
               <span slot="title">系统首页</span>
@@ -34,7 +35,7 @@
                 <span>用户管理</span>
               </template>
               <el-menu-item-group>
-                <el-menu-item index="1-1">管理员信息</el-menu-item>
+                <el-menu-item index="/about">管理员信息</el-menu-item>
                 <el-menu-item index="/user">用户信息</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
@@ -50,40 +51,44 @@
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main><router-view /></el-main>
+        <el-main>
+          <router-view />
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
-//边界线的原因是我们的菜单标签中有一个默认的边框线属性 // !important
-:声明当前这个设置是最高优先级,强制性
-<style>
-.el-menu {
-  border-right: none !important;
-}
-</style>
 
 <script>
 export default {
   data() {
     return {
-      // 1. 定义user对象（根据你的实际用户类型修改）
-      user: {
-        type: "role_admin", // 或 'role_student'、'role_teacher'等
-      },
-    };
+      //获取到当前登陆用户信息
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
   },
   methods: {
-    // 2. 定义handleOpen和handleClose方法
+    loginout() {
+      //清除当前浏览器的登录状态信息
+      localStorage.removeItem("user")
+      //跳转到登录页
+      setTimeout(() => {
+        this.$router.push("/login")
+      }, 1000)
+    },
     handleOpen(key, keyPath) {
-      console.log("菜单展开:", key, keyPath);
-      // 这里可以添加菜单展开时的逻辑
+      console.log(key, keyPath);
     },
     handleClose(key, keyPath) {
-      console.log("菜单关闭:", key, keyPath);
-      // 这里可以添加菜单关闭时的逻辑
-    },
-  },
-};
+      console.log(key, keyPath);
+    }
+  }
+}
 </script>
+
+<style lang="less">
+.myHeader {
+  background-color: #2b2b2b;
+}
+</style>
---
+++ vue_cv/vue_ding/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>这里是登录页面</h1>
+  </div>
+</template>
+<script>    
+  export default {
+    name: 'login',
+    data() {
+      return {
+      }
+    },
+    // 在页面加载时执行
+    created() {
+    },
+    methods: {
+    },
+  }
+</script>
+<style>
+
+</style>
