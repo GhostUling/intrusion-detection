a0e41c1816f878277f4e7dff9e85d4ea90f80374
Echo-Vibes <18391764356@163.com>
2025-12-30 19:39:45 +0800

    添加白名单

--- spring_001/src/main/java/com/ding/spring_001/config/WebConfig.java
+++ spring_001/src/main/java/com/ding/spring_001/config/WebConfig.java
@@ -18,7 +18,8 @@ public class WebConfig implements WebMvcConfigurer {//WebMvcConfigurer 是 Sprin
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**").excludePathPatterns("/api/user/login",
-                "/api/user/register"
+                "/api/user/register",
+                "/api/book/**"
         );
     }
 
